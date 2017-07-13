6211670c23a108bc71905fb69fe0b81c4b81f68b
majickdave <majickdave@gmail.com>
2017-07-13 00:41:01 -0700

    added interactive vis

--- sketch.js
+++ sketch.js
@@ -11,18 +11,26 @@ var radii = [];
 var gravity = [];
 var friction, damping;
 
+var number;
 var img;
 var images = []; 
 
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(1200,800);
+  background(255);
 
-  img = loadImage("resources/cool.png");
+  // number = Math.floor(randomInt(1,2176));
+
+  // img = loadImage("assets/images/48x48/".concat(number,".png"));
+  pos = [];
+  spd = [];
+  number = Math.floor(randomInt(1,2176));
+  img = loadImage("assets/images/48x48/".concat(number,".png"));
 
   // particle setup
   for(var i=0; i<count; i++){
-    pos.push(new p5.Vector(width/2, 0));
+    pos.push(new p5.Vector(mouseX, mouseY));
     spd.push(new p5.Vector(-2 + Math.random()*4, Math.random()*2));
     radii.push(random(5, 10));
     gravity.push(random(.01, .06));
@@ -38,17 +46,20 @@ function draw() {
   background(255);
   // fill(100, 255, 30);
   
-  
   for(var i=0; i<count; i++){
   	// kiwi = new image(img, width/2, height/2);
     // ellipse(pos[i].x, pos[i].y, radii[i]*2, radii[i]*2);
-    image(img, pos[i].x, pos[i].y, radii[i]*4, radii[i]*4);
+    
+    image(img, pos[i].x, pos[i].y, radii[i]*6, radii[i]*6);
       pos[i].x += spd[i].x;
       spd[i].y += gravity[i];
       pos[i].y += spd[i].y;
       checkCollisions(pos[i], spd[i], radii[i]);
   }
+  fill(255);
+  textSize(64);
 
+  text("Welcome to Music Translational Software", width/2, height/2, width/6, height/3);
 
 }
 
@@ -66,3 +77,13 @@ function checkCollisions(pos, spd, rad){
    spd.x *= friction;
  }
 }
+
+function randomInt(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function touchStarted() {
+  setup();
+
+}
+
